0f07dfdd5c81bece506afb0f06e2ac989f0e9c23
avocad5 <63831073+avocad5@users.noreply.github.com>
2020-12-29 17:30:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ for(var i = 0; i<projectname.length; i++){ //버튼 생성
   if(i<2){
   document.getElementsByClassName('project')[0].innerHTML += `<a href="${projectlink[i]}"><img src="${i-2}.png"><div><p class="pname"><strong>${projectname[i]}</strong></p><p class="pment">${projectment[i]}</p></div></a>`
 } else{
-   document.getElementsByClassName('project')[0].innerHTML += `<a href="${projectlink[i]}"><img src="${i-3}.png"><div><p class="pname"><strong>${projectname[i]}</strong></p><p class="pment">${projectment[i]}</p></div></a>`
+   document.getElementsByClassName('project')[0].innerHTML += `<a href="${projectlink[i]}"><img src="${i-1}.png"><div><p class="pname"><strong>${projectname[i]}</strong></p><p class="pment">${projectment[i]}</p></div></a>`
 }
 }
